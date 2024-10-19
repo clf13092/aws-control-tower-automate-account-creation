ecea9c9d3a547480362167395367e4067c0bd588
clf13092 <ryusugi777@gmail.com>
2024-10-20 00:42:25 +0900

    actionsテスト9

--- script/check-account-bulling/check-account-bulling.go
+++ script/check-account-bulling/check-account-bulling.go
@@ -1,74 +1,86 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "log"
-    "os"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
 
-    "github.com/aws/aws-lambda-go/lambda"
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/budgets"
-    "github.com/aws/aws-sdk-go/service/organizations"
-    "github.com/aws/aws-sdk-go/service/sns"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/budgets"
+	"github.com/aws/aws-sdk-go/service/organizations"
+	"github.com/aws/aws-sdk-go/service/sns"
 )
 
 func handler(ctx context.Context) {
-    // AWSセッションを作成
-    sess := session.Must(session.NewSession())
+	// AWSセッションを作成
+	sess := session.Must(session.NewSession())
 
-    // AWS Organizationsのクライアントを生成
-    org := organizations.New(sess)
+	// AWS Organizationsのクライアントを生成
+	org := organizations.New(sess)
 
-    // AWS Organizationsの全アカウントを取得
-    accounts, err := org.ListAccounts(&organizations.ListAccountsInput{})
-    if err != nil {
-        log.Fatalf("Failed to list accounts: %v", err)
-    }
+	// AWS Organizationsの全アカウントを取得
+	accounts, err := org.ListAccounts(&organizations.ListAccountsInput{})
+	if err != nil {
+		log.Fatalf("Failed to list accounts: %v", err)
+	}
 
-    // AWS Budgetsのクライアントを生成
-    budgetsClient := budgets.New(sess)
+	// AWS Budgetsのクライアントを生成
+	budgetsClient := budgets.New(sess)
 
-    // 予算の基準を取得
-    budgetLimit := 50000.0
+	// 予算の基準を取得
+	budgetLimit := 50000.0
 
-    // SNSクライアントを生成
-    snsClient := sns.New(sess)
+	// SNSクライアントを生成
+	snsClient := sns.New(sess)
 
-    for _, account := range accounts.Accounts {
-        // アカウントの予算を取得
-        budget, err := budgetsClient.DescribeBudget(&budgets.DescribeBudgetInput{
-            AccountId:  aws.String(*account.Id),
-            BudgetName: aws.String("MonthlyBudget"),
-        })
-        if err != nil {
-            log.Printf("Failed to describe budget for account %s: %v", *account.Id, err)
-            continue
-        }
+	for _, account := range accounts.Accounts {
+		// アカウントの予算を取得
+		budget, err := budgetsClient.DescribeBudget(&budgets.DescribeBudgetInput{
+			AccountId:  aws.String(*account.Id),
+			BudgetName: aws.String("MonthlyBudget"),
+		})
+		if err != nil {
+			log.Printf("Failed to describe budget for account %s: %v", *account.Id, err)
+			continue
+		}
+		if budget.Budget.BudgetLimit != nil && budget.Budget.BudgetLimit.Amount != nil {
+			amountStr := *budget.Budget.BudgetLimit.Amount
+			amount, err := strconv.ParseFloat(amountStr, 64)
+			if err != nil {
+				log.Printf("Failed to parse budget amount for account %s: %v", *account.Id, err)
+				continue
+			}
 
-        // 予算の基準を超えた場合は通知する
-        if *budget.Budget.BudgetLimit.Amount > budgetLimit {
-            sendNotification(snsClient, *account.Id, "Budget Exceeded", "The budget of account "+*account.Id+" has exceeded the limit")
-        } else if *budget.Budget.BudgetLimit.Amount/2 > budgetLimit {
-            sendNotification(snsClient, *account.Id, "Budget Exceeded", "The budget of account "+*account.Id+" has exceeded half of the limit")
-        }
-    }
+			// 予算の基準を超えた場合は通知する
+			if amount > budgetLimit {
+				sendNotification(snsClient, *account.Id, "Budget Exceeded", "The budget of account "+*account.Id+" has exceeded the limit")
+			} else if amount/2 > budgetLimit {
+				sendNotification(snsClient, *account.Id, "Budget Exceeded", "The budget of account "+*account.Id+" has exceeded half of the limit")
+			} else {
+				log.Printf("No budget limit set for account %s", *account.Id)
+			}
+
+		}
+	}
 }
 
 func sendNotification(snsClient *sns.SNS, accountId, subject, message string) {
-    _, err := snsClient.Publish(&sns.PublishInput{
-        TopicArn: aws.String(os.Getenv("SNS_TOPIC_ARN")),
-        Message:  aws.String(message),
-        Subject:  aws.String(subject),
-    })
-    if err != nil {
-        log.Printf("Failed to send notification for account %s: %v", accountId, err)
-    } else {
-        fmt.Printf("Notification sent for account %s: %s\n", accountId, message)
-    }
+	_, err := snsClient.Publish(&sns.PublishInput{
+		TopicArn: aws.String(os.Getenv("SNS_TOPIC_ARN")),
+		Message:  aws.String(message),
+		Subject:  aws.String(subject),
+	})
+	if err != nil {
+		log.Printf("Failed to send notification for account %s: %v", accountId, err)
+	} else {
+		fmt.Printf("Notification sent for account %s: %s\n", accountId, message)
+	}
 }
 
 func main() {
-    lambda.Start(handler)
+	lambda.Start(handler)
 }
